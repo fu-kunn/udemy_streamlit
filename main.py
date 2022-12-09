d3fab45411d6c2991dc7a29a03ab6e44aabd826f
fu-kunn <sky53908@gmail.com>
2022-12-09 10:20:43 +0900

    [Add]マークダウン記法

--- main.py
+++ main.py
@@ -7,8 +7,22 @@ st.title('streamlit 超入門')
 st.write('DataFrame')
 
 df = pd.DataFrame({
-  '1列目': [1, 2, 3, 4],
+  '1列目': [1, 10, 3, 4],
   '2列目': [10, 20, 30, 40]
 })
 
-st.write(df)
+# writeではなくdataframeにすると指定できる引数が増える
+st.dataframe(df.style.highlight_max(axis=0), width=200, height=500)
+
+# マークダウン記法
+"""
+# 章
+## 節
+### 項
+
+```python
+import streamlit as st
+import numpy as np
+import pandas as pd
+```
+"""
