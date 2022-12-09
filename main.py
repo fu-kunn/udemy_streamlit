3cd00bf2013c94d9d61fead699bb70dcdff5c70c
fu-kunn <sky53908@gmail.com>
2022-12-09 11:17:00 +0900

    [Add]画像の表示

--- main.py
+++ main.py
@@ -1,28 +1,46 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+from PIL import Image
 
 st.title('streamlit 超入門')
 
-st.write('DataFrame')
+st.write('DisplayImage')
 
-df = pd.DataFrame({
-  '1列目': [1, 10, 3, 4],
-  '2列目': [10, 20, 30, 40]
-})
+img = Image.open('mulsol.JPG')
+st.image(img, caption='MulSol', use_column_width=True)
 
-# writeではなくdataframeにすると指定できる引数が増える
+_="""
+streamlitでの複数行コメントの方法
+"""
+
+_="""
+'lat', 'lon'→緯度・経度
+[35.69, 139,79]新宿付近の緯度と経度を足してその付近を表している
+"""
+# df = pd.DataFrame(
+#   np.random.rand(100, 2)/[50, 50] + [35.69, 139.79],
+#   columns=['lat', 'lon']
+# )
+# st.line_chart(df)
+# st.area_chart(df)
+# st.bar_chart(df)
+# st.map(df)
+
+_="""
+writeではなくdataframeにすると指定できる引数が増える
 st.dataframe(df.style.highlight_max(axis=0), width=200, height=500)
+"""
 
 # マークダウン記法
-"""
-# 章
-## 節
-### 項
+# """
+# # 章
+# ## 節
+# ### 項
 
-```python
-import streamlit as st
-import numpy as np
-import pandas as pd
-```
-"""
+# ```python
+# import streamlit as st
+# import numpy as np
+# import pandas as pd
+# ```
+# """
