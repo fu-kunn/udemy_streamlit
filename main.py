84df1ecfd8e6e5078356495f2d9d3dabc72b1a47
fu-kunn <sky53908@gmail.com>
2022-12-10 09:55:13 +0900

    [Add]10-セレクトボックスの作成

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ st.title('streamlit 超入門')
 
 st.write('DisplayImage')
 
-if st.checkbox('Show Image'):
-    img = Image.open('mulsol.JPG')
-    st.image(img, caption='MulSol', use_column_width=True)
+option = st.selectbox(
+    'あなたが好きな数字を教えてください',
+    list(range(1, 11))
+)
+
+'あなたが好きな数字は', option, 'です。'
+
+
+# if st.checkbox('Show Image'):
+#     img = Image.open('mulsol.JPG')
+#     st.image(img, caption='MulSol', use_column_width=True)
 
 _="""
 streamlitでの複数行コメントの方法
