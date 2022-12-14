1ed841b8d95b6dc33e492eb2e7ee45adcc675b90
fu-kunn <sky53908@gmail.com>
2022-12-14 20:12:11 +0900

    [Add]データ構造の確認

--- kabuka.py
+++ kabuka.py
@@ -1,4 +1,4 @@
-# import pandas as pd
+import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
 
@@ -6,6 +6,8 @@ import yfinance as yf
 
 # AAPL=ティッカーシンボル
 aapl = yf.Ticker('AAPL')
+muzi = yf.Ticker('7453.T')
 days = 20 
 hist = aapl.history(period=f'{days}d')
-print(hist)
+hist_muzi = muzi.history(period=f'{days}d')
+print(pd.concat([hist, hist_muzi], axis=1).head())
