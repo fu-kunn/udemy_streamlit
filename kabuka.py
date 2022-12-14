78dfd5ad7f2b5a5043b30d21fcdcd92337b336b4
fu-kunn <sky53908@gmail.com>
2022-12-15 08:17:52 +0900

    [Start]21-株価周辺状況の取得

--- kabuka.py
+++ kabuka.py
@@ -3,17 +3,36 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 
 # AAPL=ティッカーシンボル
-aapl = yf.Ticker('AAPL')
-muzi = yf.Ticker('7453.T')
-days = 20 
-hist = aapl.history(period=f'{days}d')
-hist_muzi = muzi.history(period=f'{days}d')
+# aapl = yf.Ticker('AAPL')
+# muzi = yf.Ticker('7453.T')
+
+# hist = aapl.history(period=f'{days}d')
+# hist_muzi = muzi.history(period=f'{days}d')
 # print(pd.concat([hist, hist_muzi], axis=1).head())
 
-hist.index = hist.index.strftime('%d %B %Y')
-hist = hist[['Close']]
-hist.columns = ['apple']
-hist.head()
-hist.T
-hist.index.name = 'Name'
-print(hist)
+# days = 20
+# # FB→METAに変更
+# tickers = {
+#     'apple': 'AAPL',
+#     'facebook': 'META',
+#     'google': 'GOOGL',
+#     'microsoft': 'MSFT',
+#     'muzirushi': '7453.T'
+# }
+# # companyの中にキーが入ってくる（apple, facebook）
+# df = pd.DataFrame()
+# for company in tickers.keys():
+#     tkr = yf.Ticker(tickers[company])
+#     hist = tkr.history(period=f'{days}d')
+#     hist.index = hist.index.strftime('%d %B %Y')
+#     # 終値の指定
+#     hist = hist[['Close']]
+#     hist.columns = [company]
+#     hist = hist.T
+#     hist.index.name = 'Name'
+#     df = pd.concat([df, hist])
+
+# print(df)
+
+aapl = yf.Ticker('AAPL')
+print(aapl.info.head())
