8bcfb9e79245b63450a5409ef82f7880f64a00fe
fu-kunn <sky53908@gmail.com>
2022-12-14 15:49:19 +0900

    [Add]日数を指定してデータ取得

---
+++ kabuka.py
@@ -0,0 +1,11 @@
+# import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
+
+# %matplotlib inline
+
+# AAPL=ティッカーシンボル
+aapl = yf.Ticker('AAPL')
+days = 20 
+hist = aapl.history(period=f'{days}d')
+print(hist)
