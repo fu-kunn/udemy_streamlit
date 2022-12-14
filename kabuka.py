3df850286eb6fcb414c971de4d76ed64a964549b
fu-kunn <sky53908@gmail.com>
2022-12-14 20:24:28 +0900

    [Add]日付に対応する表の作成

--- kabuka.py
+++ kabuka.py
@@ -2,12 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
 
-# %matplotlib inline
-
 # AAPL=ティッカーシンボル
 aapl = yf.Ticker('AAPL')
 muzi = yf.Ticker('7453.T')
 days = 20 
 hist = aapl.history(period=f'{days}d')
 hist_muzi = muzi.history(period=f'{days}d')
-print(pd.concat([hist, hist_muzi], axis=1).head())
+# print(pd.concat([hist, hist_muzi], axis=1).head())
+
+hist.index = hist.index.strftime('%d %B %Y')
+hist = hist[['Close']]
+hist.columns = ['apple']
+hist.head()
+hist.T
+hist.index.name = 'Name'
+print(hist)
