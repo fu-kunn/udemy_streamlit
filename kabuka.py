8e9d198c941feda9eb6b7845ee680f57763e42ef
fu-kunn <sky53908@gmail.com>
2022-12-15 09:11:02 +0900

    [Start]22-グラフ化のためのデータ整形

--- kabuka.py
+++ kabuka.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
+import altair as alt
 
 # AAPL=ティッカーシンボル
 # aapl = yf.Ticker('AAPL')
@@ -10,29 +11,44 @@ import yfinance as yf
 # hist_muzi = muzi.history(period=f'{days}d')
 # print(pd.concat([hist, hist_muzi], axis=1).head())
 
-# days = 20
-# # FB→METAに変更
-# tickers = {
-#     'apple': 'AAPL',
-#     'facebook': 'META',
-#     'google': 'GOOGL',
-#     'microsoft': 'MSFT',
-#     'muzirushi': '7453.T'
-# }
-# # companyの中にキーが入ってくる（apple, facebook）
-# df = pd.DataFrame()
-# for company in tickers.keys():
-#     tkr = yf.Ticker(tickers[company])
-#     hist = tkr.history(period=f'{days}d')
-#     hist.index = hist.index.strftime('%d %B %Y')
-#     # 終値の指定
-#     hist = hist[['Close']]
-#     hist.columns = [company]
-#     hist = hist.T
-#     hist.index.name = 'Name'
-#     df = pd.concat([df, hist])
+days = 20
+# FB→METAに変更
+tickers = {
+    'apple': 'AAPL',
+    'facebook': 'META',
+    'google': 'GOOGL',
+    'microsoft': 'MSFT',
+    'muzirushi': '7453.T'
+}
+# companyの中にキーが入ってくる（apple, facebook）
+df = pd.DataFrame()
+for company in tickers.keys():
+    tkr = yf.Ticker(tickers[company])
+    hist = tkr.history(period=f'{days}d')
+    hist.index = hist.index.strftime('%d %B %Y')
+    # 終値の指定
+    hist = hist[['Close']]
+    hist.columns = [company]
+    hist = hist.T
+    hist.index.name = 'Name'
+    df = pd.concat([df, hist])
 
-# print(df)
+companies = ['apple', 'facebook']
+data = df.loc[companies]
+print(data)
 
-aapl = yf.Ticker('AAPL')
-print(aapl.info.head())
+
+
+
+
+_="""
+21-グラフ化
+グラフ化したものを出力できない
+print(aapl.actions['Stock Splits'].plt)→×
+actionsは公式ドキュメント参考
+"""
+# aapl = yf.Ticker('AAPL')
+# # print(aapl.info)
+# aapl.actions.head()
+# # plt.show(aapl.dividends.plot())
+# aapl.actions['Stock Splits'].plt
