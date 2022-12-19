a425f0a78c9b4e20b0a8827213116ce7fe272bb6
fu-kunn <sky53908@gmail.com>
2022-12-19 20:42:03 +0900

    [Add]26-株価範囲のサイドバー

--- kabuka.py
+++ kabuka.py
@@ -12,14 +12,37 @@ import altair as alt
 # hist_muzi = muzi.history(period=f'{days}d')
 # print(pd.concat([hist, hist_muzi], axis=1).head())
 
+# タイトル
 st.title('株価可視化アプリ')
 
+# サイドバー
 st.sidebar.write("""
 # 株価
 こちらは株価可視化ツールです。以下のオプションから表示日数をして下さい。
 """)
 
-days = 20
+st.sidebar.write("""
+## 表示日数選択
+""")
+
+days = st.sidebar.slider('日数', 1, 50, 20)
+
+st.sidebar.write("""
+## 株価の範囲指定
+""")
+ymin, ymax = st.sidebar.slider(
+    '範囲を指定して下さい',
+    0.0, 3500.0, (0.0, 3500.0)
+)
+
+# メインフォーム
+st.write(f"""
+### 過去 **{days}日間** の株価 
+""")
+
+
+
+# days = 20
 # FB→METAに変更
 tickers = {
     'facebook': 'META',
@@ -28,43 +51,52 @@ tickers = {
     'microsoft': 'MSFT',
     'muzirushi': '7453.T'
 }
+
+df = get_data(days, tickers)
+
+_="""
+@st.cache→キャッシュを溜め込むことができて、処理を高速化できる
+"""
 # companyの中にキーが入ってくる（apple, facebook）
-df = pd.DataFrame()
-for company in tickers.keys():
-    tkr = yf.Ticker(tickers[company])
-    hist = tkr.history(period=f'{days}d')
-    hist.index = hist.index.strftime('%d %B %Y')
-    # 終値の指定
-    hist = hist[['Close']]
-    hist.columns = [company]
-    hist = hist.T
-    hist.index.name = 'Name'
-    df = pd.concat([df, hist])
+@st.cache
+def get_data(days, tickers):
+    df = pd.DataFrame()
+    for company in tickers.keys():
+        tkr = yf.Ticker(tickers[company])
+        hist = tkr.history(period=f'{days}d')
+        hist.index = hist.index.strftime('%d %B %Y')
+        # 終値の指定
+        hist = hist[['Close']]
+        hist.columns = [company]
+        hist = hist.T
+        hist.index.name = 'Name'
+        df = pd.concat([df, hist])
+    return df
 
 _="""
 locは行名と列名で位置を指定、ilocは行番号と列番号で位置を指定
 """
-companies = ['apple', 'facebook']
-data = df.loc[companies]
-data = data.sort_index()
-data = data.T.reset_index()
-data = data.head()
-data = pd.melt(data, id_vars=['Date']).rename(
-  columns={'value': 'Stock Prices'}
-)
+# companies = ['apple', 'facebook']
+# data = df.loc[companies]
+# data = data.sort_index()
+# data = data.T.reset_index()
+# data = data.head()
+# data = pd.melt(data, id_vars=['Date']).rename(
+#   columns={'value': 'Stock Prices'}
+# )
 
-ymin, ymax = 120, 160
-chart = (
-  alt.Chart(data)
-  .mark_line(opacity=0.8, clip=True)
-  .encode(
-    x="Date:T",
-    y=alt.Y("Stock Prices:Q", stack=None, scale=alt.Scale(domain=[ymin, ymax])),
-    color='Name:N'
-  )
-)
-st.altair_chart(chart, use_container_width=True)
-# st.altair_chart(chart)
+# ymin, ymax = 120, 160
+# chart = (
+#   alt.Chart(data)
+#   .mark_line(opacity=0.8, clip=True)
+#   .encode(
+#     x="Date:T",
+#     y=alt.Y("Stock Prices:Q", stack=None, scale=alt.Scale(domain=[ymin, ymax])),
+#     color='Name:N'
+#   )
+# )
+# st.altair_chart(chart, use_container_width=True)
+# # st.altair_chart(chart)
 
 
 
