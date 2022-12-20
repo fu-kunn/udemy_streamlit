3760f4bd64d4d020648525ee12bca378c5b17740
fu-kunn <sky53908@gmail.com>
2022-12-20 21:59:00 +0900

    [Add]関数の作成

--- jkabuka.py
+++ jkabuka.py
@@ -3,14 +3,25 @@ import yfinance as yf
 import pandas as pd
 import altair as alt
 
-muzirushi = yf.Ticker('7453.T')
-days = 20
-hist = muzirushi.history(period=f'{days}d')
+def get_data(days, tickers):
+    df = pd.DataFrame()
+    for company in tickers.keys():
+        tkr = yf.Ticker(tickers[company])
+        hist = tkr.history(period=f'{days}d')
+        hist.index = hist.index.strftime('%d %B %Y')
+        hist = hist[['Close']]
+        hist.columns = [company]
+        hist = hist.T
+        hist.index.name = 'Name'
+        df = pd.concat([df, hist])
+    return df
 
-hist.index = hist.index.strftime('%d %B %Y')
-hist = hist[['Close']]
-hist.cloumns = ['無印良品']
-hist.head()
-hist = hist.T
-hist.index.name = 'Name'
-print(hist)
+days = 20
+tickers = {
+    'muzirushi': '7453.T',
+    'rakuten': '4755.T',
+    'sandora': '9989.T',
+    'toyota': '7203.T',
+}
+
+st.write(get_data(days, tickers))
